ed135125434d57d881ade3d9a92da089cd951604
Joe Weakley <joew@samjoe.com>
2017-12-17 13:10:57 -0800

    WIP Demo build

---
+++ Flashlight Tag/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LookAtCamera: MonoBehaviour  {
+	Transform mainCamera;   
+
+	void Start() {
+		//Set the Main Camera as the target
+		mainCamera = Camera.main.transform;
+	}
+
+	void LateUpdate() {
+		if (mainCamera == null) {
+			return;
+		}
+
+		//Apply the rotation needed to look at the camera. Note, since pointing a UI text element
+		//at the camera makes it appear backwards, we are actually pointing this object
+		//directly *away* from the camera.
+		transform.rotation = Quaternion.LookRotation (transform.position - mainCamera.position);
+	}
+}
---
+++ Flashlight Tag/Assets/Scripts/TagEffectManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TagEffectManager : MonoBehaviour 
+{
+	[SerializeField] Light flashLight;
+	[SerializeField] AudioSource gunAudio;
+	[SerializeField] GameObject impactPrefab;
+
+	ParticleSystem impactEffect;
+
+	//Create the impact effect for our shots
+	public void Initialize()
+	{
+//		impactEffect = Instantiate(impactPrefab).GetComponent<ParticleSystem>();
+	}
+
+	//Play muzzle flash and audio
+	public void PlayTagEffects()
+	{
+		// Adjust flashlight intensity
+		gunAudio.Stop();
+		gunAudio.Play();
+	}
+
+	//Play impact effect and target position
+	public void PlayImpactEffect(Vector3 impactPosition)
+	{
+//		impactEffect.transform.position = impactPosition;   
+//		impactEffect.Stop();
+//		impactEffect.Play();
+	}
+}
